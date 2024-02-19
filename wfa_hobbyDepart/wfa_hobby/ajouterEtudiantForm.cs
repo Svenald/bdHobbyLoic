0ab7eb5e8b34a8f6da24ec7b24730fa081340828
l.mousques <105661530+Svenald@users.noreply.github.com>
2024-02-19 15:16:55 -0500

    Ajout ManagerEtudiant + AjouterEtudiant

--- wfa_hobbyDepart/wfa_hobby/ajouterEtudiantForm.cs
+++ wfa_hobbyDepart/wfa_hobby/ajouterEtudiantForm.cs
@@ -22,8 +22,39 @@ namespace wfa_hobby
            
         }
 
+        private bool textBoxRemplis()
+        {
+            return(prenomTextBox.Text != "" && nomTextBox.Text != "" && cellulaireTextBox.Text != "");
+        }
+        private Etudiant RemplirColonneEtudiant()
+        {
+            Etudiant etudiant = new Etudiant();
+            etudiant.Prenom = prenomTextBox.Text;
+            etudiant.Nom = nomTextBox.Text;
+            etudiant.Cellulaire = cellulaireTextBox.Text;
+            etudiant.Humour = (int)humourNumericUpDown.Value;
+            etudiant.No_provenance = 1;
+            return etudiant;
+        }
+
         private void ajouterButton_Click(object sender, EventArgs e)
         {
+            ManagerEtudiant managerEtudiant = new ManagerEtudiant();
+            Etudiant etudiant = new Etudiant();
+            int nombreLignesAffectees = 0;
+            try
+            {
+                if(textBoxRemplis())
+                {
+                    etudiant = RemplirColonneEtudiant();
+                    nombreLignesAffectees = managerEtudiant.AjouterEtudiant(etudiant);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
         }
     }
 }
